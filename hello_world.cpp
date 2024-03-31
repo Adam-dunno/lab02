9c5a65c316008989f7b9ad65de559e1b51229b07
Adam-dunno <goshaivanov2019@mail.ru>
2024-03-31 15:38:23 +0300

    Added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::string name;
     std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::cin >> name; // write here your name, please!
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
